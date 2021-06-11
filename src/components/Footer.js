5cb5deb4f86b2609bb9f65694bd2f02deebdab85
Kieran <kieranderfus@gmail.com>
2021-06-11 03:00:28 -0400

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+      <footer>
+        <p>Powered by</p>
+        <img src="/images/frame-light-footer.svg" key="footer-light-img" alt="footer-img"/>
+      </footer>
+  )
+}
+
+export default Footer
